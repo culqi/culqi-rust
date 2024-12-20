bb6119ba0374dcf4e0a1b887b8168d0883cbac40
Brando Javier  Carquin Mendocilla <brando.carquin@culqi.com>
2024-12-20 12:14:03 -0500

    error class

--- src/culqi/order.rs
+++ src/culqi/order.rs
@@ -1,17 +1,16 @@
-use crate::utils::urls::ORDER_URL;
 use crate::client::Client;
+use crate::utils::urls::ORDER_URL;
 extern crate serde_json;
 
-use serde::Serialize;
 use anyhow::Result;
+use hyper::StatusCode;
+use serde::Serialize;
 use serde_json::json;
-use warp::{reply::Response, Reply};
-
+use warp::{reply, Reply};
 #[derive(Debug, Serialize)]
-pub struct Order {
-}
+pub struct Order {}
 
-#[derive(Debug)]  // Agregar la derivación de Debug
+#[derive(Debug)] // Agregar la derivación de Debug
 pub struct CustomRejection {
     status: warp::http::StatusCode,
     body: String,
@@ -26,39 +25,51 @@ impl CustomRejection {
 impl warp::reject::Reject for CustomRejection {}
 
 impl Order {
-    pub async fn create<T: Serialize>(client: &Client, order_request: &T, custom_header: Option<&str>) -> Result<warp::reply::Response, warp::Rejection> {
+    pub async fn create<T: Serialize>(
+        client: &Client,
+        order_request: &T,
+        custom_header: Option<&str>,
+    ) -> Result<warp::reply::Response, warp::Rejection> {
         match client.post(ORDER_URL, order_request, custom_header).await {
             Ok((response_text, status_code)) => {
+                // Si la petición es exitosa, retorna la respuesta con su código de estado
                 let response = create_warp_response(response_text, status_code);
-                Ok(response)
+                Ok(response) // Aquí se retorna un Result<warp::reply::Response, warp::Rejection>
             }
             Err((error_message, status_code)) => {
-                // Aquí creamos un cuerpo JSON para el error
-                let error_body = json!({
-                    "error": error_message,
-                    "status": status_code,
-                }).to_string();  // Convierte el error a String
-    
-                // Creamos la respuesta de error con el cuerpo y código de estado
-                let error_response = create_warp_response(error_body.clone(), status_code);
-    
-                // Rechazo con el código de estado y el cuerpo del error
-                Err(warp::reject::custom(CustomRejection::from_status_code(error_response.status(), error_body)))
+                let status =
+                    StatusCode::from_u16(status_code).unwrap_or(StatusCode::INTERNAL_SERVER_ERROR);
+                Err(warp::reject::custom(CustomRejection::from_status_code(
+                    status,
+                    error_message,
+                )))
             }
         }
     }
 
-    pub async fn get(client: &Client, id: &str, custom_header: Option<&str>) -> Result<(String, u16)> {
+    pub async fn get(
+        client: &Client,
+        id: &str,
+        custom_header: Option<&str>,
+    ) -> Result<(String, u16)> {
         let response = client.get(ORDER_URL, &id, custom_header).await?;
         Ok(response)
     }
 
-    pub async fn all(client: &Client, params: &str, custom_header: Option<&str>) -> Result<(String, u16)> {
+    pub async fn all(
+        client: &Client,
+        params: &str,
+        custom_header: Option<&str>,
+    ) -> Result<(String, u16)> {
         let response = client.all(ORDER_URL, params, custom_header).await?;
         Ok(response)
     }
 
-    pub async fn delete(client: &Client, id: &str, custom_header: Option<&str>) -> Result<(String, u16)> {
+    pub async fn delete(
+        client: &Client,
+        id: &str,
+        custom_header: Option<&str>,
+    ) -> Result<(String, u16)> {
         let response = client.delete(ORDER_URL, &id, custom_header).await?;
         Ok(response)
     }
@@ -71,6 +82,8 @@ fn create_warp_response(body: String, status_code: u16) -> warp::reply::Response
     if status.is_success() {
         warp::reply::with_status(warp::reply::html(body), status).into_response()
     } else {
-        warp::reply::with_status(warp::reply::json(&json!({ "message": body })), status).into_response()
+        let error_body = json!({ "message": body });
+        let warp_response = warp::reply::with_status(warp::reply::json(&error_body), status);
+        warp_response.into_response()
     }
 }
